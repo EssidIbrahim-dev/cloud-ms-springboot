975e54709cbb2d61a813289e54b5fc6803464f4b
EssidIbrahim-dev <ibraslime94@gmail.com>
2021-10-25 00:18:38 +0100

    microservices

--- src/main/java/com/essid_dev/os/api/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.essid_dev.os.api.repository;
-
-import com.essid_dev.os.api.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
