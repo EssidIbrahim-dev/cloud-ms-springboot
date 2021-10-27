d4d836389712041c76908f0a6ffa3b1829458849
aymenhergli <60631551+aymenhergli@users.noreply.github.com>
2021-10-27 11:59:46 +0200

    Update FactureMicroserviceApplication.java

--- FactureMs/src/main/java/com/example/demo/FactureMicroserviceApplication.java
+++ FactureMs/src/main/java/com/example/demo/FactureMicroserviceApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackageClasses = FactureRestAPI.class)
 public class FactureMicroserviceApplication {
 
 	public static void main(String[] args) {
