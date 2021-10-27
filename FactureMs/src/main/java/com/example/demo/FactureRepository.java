b06ec4c5171183392b7eaebf97e1a8132be05178
EssidIbrahim-dev <ibraslime94@gmail.com>
2021-10-27 12:13:50 +0100

    spring microservices

--- FactureMs/src/main/java/com/example/demo/FactureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FactureRepository extends JpaRepository<FactureEntity, Integer>  {
-
-}
